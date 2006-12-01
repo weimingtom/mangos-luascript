6ce282b1eeb840be93b54dfe4090d1541d2e39bc
Derex101 <Derex101@f2ff2d62-ea21-0410-8eb0-7b0e83e86450>
2006-12-01 18:46:59 +0000

    Addapting for latest mangos revision ( 2744 )

--- src/bindings/luascript/Lua_AI.h
+++ src/bindings/luascript/Lua_AI.h
@@ -22,48 +22,58 @@ bool IsValidLuaAIState(const luabind::adl::object& ob);
 #define VISIBLE_RANGE (26.46f)
 
 class MANGOS_DLL_DECL LuaAI : public CreatureAI
-{
-private:
-	 luabind::object m_CurrentState;
+  {
+  private:
+    luabind::object m_CurrentState;
 
-	Creature* m_creature;
+    Creature* m_creature;
 
-public:
-    LuaAI(Creature* creature) : m_creature(creature) {}
-    ~LuaAI() {}
-     
-	void SetCurrentState(const luabind::object& s);
+  public:
+    LuaAI(Creature* creature) : m_creature(creature)
+    {}
+    ~LuaAI()
+    {}
 
-  //this method makes sure the current state object is valid before calling
-  // the Execute function of the Lua table it represents
-  void UpdateScriptedStateMachine();
+    void SetCurrentState(const luabind::object& s);
 
-  //change to a new state
-  void ChangeState(const luabind::object& new_state);
+    //this method makes sure the current state object is valid before calling
+    // the Execute function of the Lua table it represents
+    void UpdateScriptedStateMachine();
+
+    //change to a new state
+    void ChangeState(const luabind::object& new_state);
 
     //retrieve the current state
-    const luabind::object& CurrentState()const {return m_CurrentState;}
+    const luabind::object& CurrentState()const
+      {
+        return m_CurrentState;
+      }
 
     // Called if IsVisible(Unit *who) is true at each *who move
-    void MoveInLineOfSight(Unit *) {}
+    void MoveInLineOfSight(Unit *)
+    {}
 
     // Called at each attack of m_creature by any victim
-    void AttackStart(Unit *) {}
+    void AttackStart(Unit *)
+    {}
 
     // Called at stoping attack by any attacker
     void AttackStop(Unit *);
 
     // Called at any heal cast/item used (call non implemented in mangos)
-    void HealBy(Unit *healer, uint32 amount_healed) {}
+    void HealBy(Unit *healer, uint32 amount_healed)
+    {}
 
     // Called at any Damage from any attacker
-    void DamageInflict(Unit *healer, uint32 amount_healed) {}
+    void DamageInflict(Unit *healer, uint32 amount_healed)
+    {}
 
     // Is unit visibale for MoveInLineOfSight
     bool IsVisible(Unit *who) const
-    {
-        return !who->isStealth() && m_creature->GetDistanceSq(who) <= VISIBLE_RANGE;
-    }
+      {
+        debug_log( "LuaAI::IsVisible not implemented" );
+        return false; /*!who->isStealth() && m_creature->GetDistanceSq(who) <= VISIBLE_RANGE;*/
+      }
 
     // Called at World update tick
     void UpdateAI(const uint32);
@@ -82,21 +92,21 @@ public:
     // Cast spell
     void DoCast(Unit* victim, uint32 spelId)
     {
-        m_creature->CastSpell(victim,spelId,true);
+      m_creature->CastSpell(victim,spelId,true);
     }
 
     void DoCastSpell(Unit* who,SpellEntry *spellInfo)
     {
-        m_creature->CastSpell(who,spellInfo,true);
+      m_creature->CastSpell(who,spellInfo,true);
     }
 
     void DoSay(char const* text, uint32 language)
     {
-        m_creature->MonsterSay(text,language, m_creature->GetGUID());
+      m_creature->MonsterSay(text,language, m_creature->GetGUID());
     }
 
     void DoGoHome();
-};
+  };
 
 
 #endif //LUA_AI_H
