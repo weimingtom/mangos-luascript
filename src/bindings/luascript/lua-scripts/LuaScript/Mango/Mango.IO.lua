e1ae8eb7d7ec1a9c77f1d03ad2476d2a62ce1afb
tv2knet <tv2knet@f2ff2d62-ea21-0410-8eb0-7b0e83e86450>
2006-12-02 23:02:35 +0000

    * Fixed some bugs

--- src/bindings/luascript/lua-scripts/LuaScript/Mango/Mango.IO.lua
+++ src/bindings/luascript/lua-scripts/LuaScript/Mango/Mango.IO.lua
@@ -2,13 +2,13 @@ printf("LuaScript: Loading Mango IO library...\n")
 -- Mango Input/Output Library
 Mango.IO = {
 	printf  = function (...)
-		io.write(string.format(unpack(arg)))
-	end
+		io.write(string.format(unpack(arg)));
+	end;
 	log = function (...)
-		log_debug(string.format(unpack(arg))) 
-	end	
+		log_debug(string.format(unpack(arg)));
+	end;
 	logerror = function (...)
-		log_error(string.format(unpack(arg)))
-	end
+		log_error(string.format(unpack(arg)));
+	end;
 }
 printf("LuaScript: Loaded Mango IO library!\n")
