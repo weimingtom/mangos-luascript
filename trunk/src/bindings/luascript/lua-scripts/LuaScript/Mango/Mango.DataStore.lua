6f67741be769a071bc00459813f45cb4dcdc8e2f
tv2knet <tv2knet@f2ff2d62-ea21-0410-8eb0-7b0e83e86450>
2006-12-02 21:40:02 +0000

    * Fixed some typos

--- trunk/src/bindings/luascript/lua-scripts/LuaScript/Mango/Mango.DataStore.lua
+++ trunk/src/bindings/luascript/lua-scripts/LuaScript/Mango/Mango.DataStore.lua
@@ -4,16 +4,15 @@ Mango.DataStore = {
 	local m_DataStore = {}
 
 	get = function (...)
-		current_script ~= nil
 		if (Mango.m_DataStore[string.format(unpack(arg))] == nil) then
-			Mango.m_DataStore[string.format(unpack(arg))] = {}
+			Mango.DataStore.m_DataStore[string.format(unpack(arg))] = {}
 		end
 
-		return Mango.m_DataStore[string.format(unpack(arg))]		
+		return Mango.DataStore.m_DataStore[string.format(unpack(arg))]		
 	end
 
 	clear = function (...)
-		Mango.m_DataStore[string.format(unpack(arg))] = {}
+		Mango.DataStore.m_DataStore[string.format(unpack(arg))] = {}
 	end	
 	
 }
