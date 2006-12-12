b42dfa2a05538a5ca39adfe8be3020b0fe55b5b8
tv2knet <tv2knet@f2ff2d62-ea21-0410-8eb0-7b0e83e86450>
2006-12-12 02:29:27 +0000

    * Another update

--- src/bindings/luascript/lua-scripts/LuaScript/guards/guard_stormwind.lua
+++ src/bindings/luascript/lua-scripts/LuaScript/guards/guard_stormwind.lua
@@ -6,12 +6,12 @@ Guard_Stormwind = Mango.Scripts.New('guard_stormwind');
 Guard_Stormwind:ForceLUA(); -- Don't use the CPP version if valid
 
 -- Function prototypes (to keep it clean)
-local Guard_Stormwind_GossipHello;
-local Guard_Stormwind_GossipSelect;
-local Guard_Stormwind_SendDefaultMenu;
-local Guard_Stormwind_SendBattleMasterMenu;
-local Guard_Stormwind_SendClassTrainerMenu;
-local Guard_Stormwind_SendProfTrainerMenu;
+local Guard_Stormwind_GossipHello
+local Guard_Stormwind_GossipSelect
+local Guard_Stormwind_SendDefaultMenu
+local Guard_Stormwind_SendBattleMasterMenu
+local Guard_Stormwind_SendClassTrainerMenu
+local Guard_Stormwind_SendProfTrainerMenu
 
 -- The actual functions
 -- Send the first menu (with the selections)
