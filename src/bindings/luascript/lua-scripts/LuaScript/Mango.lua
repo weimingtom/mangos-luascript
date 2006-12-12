e658011ad62ac4bcb2d8b5f239777e98c055839d
tv2knet <tv2knet@f2ff2d62-ea21-0410-8eb0-7b0e83e86450>
2006-12-12 02:24:11 +0000

    * Added the new Mango Scripts library (EXPERIMENTAL) (NOT TESTED) * Added an EXPERIMENTAL version of guard_stormwind using the Mango Scripts library (NOT TESTED)

--- src/bindings/luascript/lua-scripts/LuaScript/Mango.lua
+++ src/bindings/luascript/lua-scripts/LuaScript/Mango.lua
@@ -3,12 +3,18 @@ printf("LuaScript: Loading the Mango library...\n")
 -- Mango, Fruit we all like :)
 --
 Mango = {
-	version = 0.01;
+	version = 0.05; -- Should increase + 0.01 on each time a library gets added
 
 	-- Input/Output Libraries
 	IO = {};
 
 	-- DataStore Libraries
 	DataStore = {};
+
+	-- Stubby Libraries
+	Stubby = {};
+
+	-- Scripts Libraries
+	Scripts = {};
 };
 printf("LuaScript: Loaded the Mango library!\n")
